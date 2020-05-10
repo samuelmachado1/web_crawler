26f5a85155ee4689b19962e6ec016db7d212bd70
Samuel Estrella <samuelmachado1@gmail.com>
2020-05-10 00:45:36 -0300

    Implementing the crawler to the microsoft community forum site

--- WebCrawler/WebCrawler/Form1.cs
+++ WebCrawler/WebCrawler/Form1.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace WebCrawler
 {
@@ -19,7 +22,35 @@ namespace WebCrawler
 
         private void AtualizarButton_Click(object sender, EventArgs e)
         {
-                 
+            var wc = new WebClient();
+            string pagina = wc.DownloadString("https://social.msdn.microsoft.com/Forums/pt-BR/home?filter=alltypes&sort=firstpostdesc");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(pagina);
+
+            dataGridView1.Rows.Clear();
+
+            string id = string.Empty;
+            string titulo = string.Empty;
+            string postagem = string.Empty;
+            string exibicao = string.Empty;
+            string resposta = string.Empty;
+            string link = string.Empty;
+
+            foreach (HtmlNode node in htmlDocument.GetElementbyId("threadList").ChildNodes)
+            {
+                if (node.Attributes.Count > 0)
+                { 
+                    id = node.Attributes["data-threadid"].Value;
+                    link = "https://social.msdn.microsoft.com/Forums/pt-BR/" + id;
+                    titulo = node.Descendants().First(x => x.Id.Equals("threadTitle_" + id)).InnerText;
+                    postagem = node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("lastpost")).InnerText.Replace("\n", "").Replace("  ", "");
+                    exibicao = node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("viewcount")).InnerText;
+                    resposta = node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("replycount")).InnerText;
+                }
+                if (!string.IsNullOrEmpty(titulo))
+                    dataGridView1.Rows.Add(titulo, postagem, exibicao, resposta, link);
+            }
         }
     }
 }
